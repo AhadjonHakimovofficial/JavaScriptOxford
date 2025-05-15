02b000c0594689d484090453618c060224a4acea
AhadjonHakimovofficial <hakimjonovahadjon5@gmail.com>
2025-05-15 19:38:14 +0500

    important

---
+++ Homeworks JS/18-Homework Js/index.js
@@ -0,0 +1,23 @@
+let fructs = [`Apple`,`Orange`,`Reaps`]
+
+let shoppingCard = fructs;
+
+shoppingCard.push(`Banana`)
+
+
+console.log(fructs.length);
+
+
+// Length = 4
+
+
+let arr = [`A`,"b"]
+
+arr.push(function(){
+    alert(this);
+return alert
+});
+
+console.log(arr.push);
+
+// [Function: push]
